ea8a092c61f78a20ebce5c47c52b3196a69273c8
Garret Tonra <gtonra89@gmail.com>
2017-02-13 18:02:57 +0000

    got the database connected to the app via the api key and url for my DB created in Mlab

---
+++ src/app/service/workout-service.ts
@@ -0,0 +1,26 @@
+/**
+ * Created by Garret on 13-Feb-17.
+ */
+/**import injectable package from angular Core*/
+/**also http modules and request options */
+
+import {Injectable} from '@angular/core';
+import {Http, Headers, RequestOptions} from '@angular/http';
+import 'rxjs/Rx'; /**is a set of libraries to compose asynchronous and event-based programs using observable collections*/
+
+@Injectable()
+export class WorkoutService{
+    http:any;
+    apiKey: String;
+    workoutUrl: String;
+
+    constructor(http:Http){
+        this.http = http;
+        this.apiKey = 'rP_Le0LqtA8BjUxqbZTi5zasXGZO2pAQ';
+        this.workoutUrl = 'https://api.mlab.com/api/1/databases/gtworkoutapp/collections/workout'
+    }
+
+    getWorkout(){
+        return this.http.get(this.workoutUrl+'?apiKey='+this.apiKey ).map(res => res.json());
+    }
+}
--- src/pages/workouts/workouts.ts
+++ src/pages/workouts/workouts.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import {WorkoutService} from "../../app/service/workout-service";
 
 @Component({
   selector: 'Workouts',
@@ -8,8 +8,12 @@ import { NavController } from 'ionic-angular';
 })
 export class WorkoutsPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private wkoutService: WorkoutService) {
 
   }
-
+  ngOnInit(){
+    this.wkoutService.getWorkout().subscribe(workouts =>
+    {console.log(workouts);
+    });
+  }
 }
