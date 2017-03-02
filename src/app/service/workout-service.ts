629c1383789ecc8dccb36d3b7eaea07f87c9cb6a
Garret Tonra <gtonra89@gmail.com>
2017-03-02 18:34:05 +0000

    got the dropdown list pulling from database and populate list instead of hardcoding workouts also adding delete button and add new exercise when not available in the list

--- src/app/service/workout-service.ts
+++ src/app/service/workout-service.ts
@@ -1,32 +1,71 @@
 /**
  * Created by Garret on 13-Feb-17.
  */
-/**import injectable package from angular Core*/
-/**also http modules and request options */
-
+/**import injectable package from angular Core
+ * also http modules and request options 
+ * is a set of libraries to compose asynchronous and 
+ * event-based programs using observable collections
+ * */
 import {Injectable} from '@angular/core';
 import {Http, Headers, RequestOptions} from '@angular/http';
-import 'rxjs/Rx'; /**is a set of libraries to compose asynchronous and event-based programs using observable collections*/
-
+import 'rxjs/Rx'; 
+/*************************************************************************************************************/
 @Injectable()
 export class WorkoutService{
     http:any;
-    apiKey: String;
-    workoutUrl: String;
+   WorkoutApiKey: String;
+    WorkoutUrl: String;
+    WorkoutListUrl:String;
+    WorkoutListApiKey:String;
 
     constructor(http:Http){
         this.http = http;
-        this.apiKey = 'rP_Le0LqtA8BjUxqbZTi5zasXGZO2pAQ';
-        this.workoutUrl = 'https://api.mlab.com/api/1/databases/gtworkoutapp/collections/workout'
+        this.WorkoutApiKey = 'rP_Le0LqtA8BjUxqbZTi5zasXGZO2pAQ';
+        this.WorkoutListApiKey = 'rP_Le0LqtA8BjUxqbZTi5zasXGZO2pAQ';
+        this.WorkoutUrl = 'https://api.mlab.com/api/1/databases/gtworkoutapp/collections/workout';
+        this.WorkoutListUrl = 'https://api.mlab.com/api/1/databases/workout2/collections/Workout2';
     }
 
-    getWorkout(){
-        return this.http.get(this.workoutUrl+'?apiKey='+this.apiKey ).map(res => res.json());
+/*************************************************************************************************************/
+
+    /**
+     * Simple Get Request to get workourts 
+     * from the database back using the api key and url
+     */
+    getWorkout(){  
+        return this.http.get(this.WorkoutUrl+'?apiKey='+this.WorkoutApiKey ).map(res => res.json());
     }
 
+/*************************************************************************************************************/
+/*************************************************************************************************************/
+
+    /**
+     * Simple Get Request to get workourts 
+     * from the database back using the api key and url
+     */
+    getWorkout1(){  
+        return this.http.get(this.WorkoutListUrl+'?apiKey='+this.WorkoutListApiKey ).map(res1 => res1.json());
+    }
+
+/*************************************************************************************************************/
+     /**
+     * create a new header using the Headers() constructor,
+     * add a new header to it using append(),   
+     * then return that header value using Post()
+     * sending the WorkoutApiKey and the workoutUrl
+     * Converts a JavaScript value to a JavaScript Object Notation (JSON) string. 
+    */
     addWorkout(workout){
+        
         var headers = new Headers();
         headers.append('Content-Type', 'application/json');
-        return this.http.post(this.workoutUrl+'?apiKey='+this.apiKey, JSON.stringify(workout),{headers:headers}).map(res => res.json());
+        return this.http.post(this.WorkoutUrl+'?apiKey='+this.WorkoutApiKey, JSON.stringify(workout),{headers:headers}).map(res => res.json());
     }
+
+    addNewExercise(newExercise){
+        var headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(this.WorkoutListUrl+'?apiKey='+this.WorkoutListApiKey, JSON.stringify(newExercise),{headers:headers}).map(res => res.json());
+    }
+
 }
--- src/pages/add-a-workout/add-a-workout.ts
+++ src/pages/add-a-workout/add-a-workout.ts
@@ -2,7 +2,7 @@
  * Created by Garret on 11-Feb-17.
  */
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, List} from 'ionic-angular';
 import {WorkoutsPage} from '../workouts/workouts';
 import {WorkoutService} from "../../app/service/workout-service";
 
@@ -11,33 +11,44 @@ import {WorkoutService} from "../../app/service/workout-service";
     templateUrl: 'add-a-workout.html'
 })
 export class AddAWorkoutPage {
-    public workoutArea : String;
+
     public Sets: String;
     public Reps : String;
-    public Exercise: String;
     public Weight: number;
     public result : any;
+    public workouts : any;
+    public Exercise : any;
 
-    constructor(public navCtrl: NavController, private workoutSrv:WorkoutService){
-
+    constructor(public navCtrl: NavController, private workoutService:WorkoutService){
+        
     }
+
+    ngOnInit(){
+    
+    this.workoutService.getWorkout1().subscribe(workouts => {
+      this.workouts = workouts;
+      workouts.toString();
+      console.log();
+    });
+  }
 
     onSubmit(){
         var workout = {
-            workoutArea : this.workoutArea,
             Sets: this.Sets,
             Reps: this.Reps,
             Weight : this.Weight,
             Exercise: this.Exercise
-
         }
 
         //add workout using service eg the database list
-        this.workoutSrv.addWorkout(workout).subscribe(data => {
+        this.workoutService.addWorkout(workout).subscribe(data => {
             this.result = data;
         });
 
         this.navCtrl.push(WorkoutsPage);
+        
     }
+
+
 
 }
